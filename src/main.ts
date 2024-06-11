35382400eeae75f387fed5e1e1c6544097e02ec7
Rolando HR <rolando167@hotmail.com>
2024-06-11 18:32:34 +0200

    RH - cors

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,9 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
-    await app.listen(3000);
+    app.enableCors({
+        origin: 'https:faztweb.com',
+    })
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
