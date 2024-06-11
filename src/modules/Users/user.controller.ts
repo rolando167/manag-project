5708841b5d6ca2748f4b12853a7d5d23471fe87c
Rolando HR <rolando167@hotmail.com>
2024-06-11 17:37:46 +0200

    RH - Guards

--- src/modules/Users/user.controller.ts
+++ src/modules/Users/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Query } from "@nestjs/common";
+import { Controller, Get, Query, UseGuards } from "@nestjs/common";
 import { ValidateuserPipe } from "./pipes/validateuser.pipe";
+import { AuthGuard } from "./Guards/auth.guard";
 
 @Controller({
     version: '1'
 })
 export class UserController {
     @Get('greet')
-    // @UseGuards(AuthGuard)
+    @UseGuards(AuthGuard)
     greet(@Query(ValidateuserPipe) query: { name: string; age: number }) {
       console.log(typeof query.age);
       console.log(typeof query.name);
